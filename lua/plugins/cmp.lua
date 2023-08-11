1af197d8e8a531850003d37872599b00e81fa5b7
Michael Lazeroff <michaeldlazeroff@gmail.com>
2023-08-10 20:11:08 -0700

    feat: cmp autopairs for functions

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -37,11 +37,16 @@ return {
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "saadparwaiz1/cmp_luasnip",
+      "windwp/nvim-autopairs"
     },
     opts = function()
       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
       local cmp = require("cmp")
       local defaults = require("cmp.config.default")()
+
+      -- a little autopairs action for functions
+      cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
+
       return {
         completion = {
           completeopt = "menu,menuone,noinsert",
@@ -52,13 +57,11 @@ return {
           end,
         },
         mapping = cmp.mapping.preset.insert({
+          ["<cr>"] = cmp.config.disable,
           ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          -- ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          -- ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
           ["<C-y"] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
@@ -86,6 +89,6 @@ return {
         },
         sorting = defaults.sorting,
       }
-    end,
+    end
   },
 }
