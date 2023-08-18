b51ddda61e7dc9664d5639ab18b9a5f36d49b144
Michael Lazeroff <michaeldlazeroff@gmail.com>
2023-08-18 15:56:12 -0700

    feat(trouble): reverse keymaps for trouble

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -6,9 +6,9 @@ return {
   keys = {
     { "]t", function() require("todo-comments").jump_next() end, desc = "Next [t]odo comment" },
     { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous [t]odo comment" },
-    { "<leader>xT", "<cmd>TodoTrouble<cr>", desc = "[t]odo (Trouble)" },
-    { "<leader>xt", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "[t]odo/fix/fixme (Trouble)" },
-    { "<leader>sT", "<cmd>TodoTelescope<cr>", desc = "[s]earch [t]odo" },
-    { "<leader>st", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "[s]earch [t]odo/fix/fixme" },
+    { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "[t]odo (Trouble)" },
+    { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "[t]odo/fix/fixme (Trouble)" },
+    { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "[s]earch [t]odo" },
+    { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "[s]earch [t]odo/fix/fixme" },
   },
 }
